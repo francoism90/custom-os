ccff3feaaff120a4b1f1700606a2168754dbee30
francoism90 <francoism90@users.noreply.github.com>
2026-01-11 16:30:28 +0100

    Enhance mkosi configuration: update kernel command line, add accounts-daemon and pacman packages, create systemd-sysupdate preset, and add zram generator configuration

--- mkosi.conf.d/04-apparmor.sh
+++ mkosi.conf.d/04-apparmor.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -eux
 
-# TODO: set cache
+# Configure AppArmor parser cache settings
+mkdir -p /etc/apparmor/earlypolicy/
+cat > /etc/apparmor/parser.conf <<'EOF'
+write-cache
+Optimize=compress-fast
+cache-loc /etc/apparmor/earlypolicy/
+EOF
 
 exit 0
